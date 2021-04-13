2d43d5db27d4ce521426cc9685ef8e69ff4701cd
Cristian Berrio <cristin.berrio@teaminernational.com>
2021-04-13 15:34:14 -0500

    Inicial commit

---
+++ src/test/java/com/example/demo/PruebaApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.controller.HolaMundoController;
+
+@SpringBootTest
+class PruebaApplicationTests {
+	
+	@Autowired
+	private HolaMundoController holaMundoController;
+
+	@Test
+	void contextLoads() {
+		String name = "pepito";
+		String response = holaMundoController.Hola(name);
+		assertEquals("Hola pepito", response);
+	}
+
+}
